cc2897d5519ec30a44b906f362c37017781172b3
qinzzz <wangqinxin2018@gmail.com>
2026-02-05 01:08:08 -0800

    Correctly export modules in routers/__init__.py to fix AttributeError

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -1,5 +1,5 @@
-from .artwork import router as artwork
-from .users import router as users
-from .collection import router as collection
-from .tag import router as tag
-from .auth import router as auth
+from . import artwork
+from . import users
+from . import collection
+from . import tag
+from . import auth
